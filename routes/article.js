bf4284a1ef9d3854142d6a9dc180f45ea69b97dc
jordankamto <jordankamto32@gmail.com>
2025-01-30 16:55:06 +0100

    added cors and updated controllers and routes

--- routes/article.js
+++ routes/article.js
@@ -4,8 +4,10 @@ const Article = require('../controllers/articles');
 
 const router = express.Router();
 
+//GET on /api/article/all
 router.get('/all', Article.getArticles);
 
+//POST on /api/article/post
 router.post('/post', Article.postArticle);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const articleRoutes = require('./routes/article');
 const app = express();
 
 app.use(bodyparser.json());
+
+//Permitting cross-site requests to update with cors()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/api/article', articleRoutes);
 
 app.listen(8080);
